2913875466b3c99aceba8416732ed09f01a40770
Stephanus8889 <stephanus888889@gmail.com>
2022-11-13 23:52:49 +0700

    Update Background Pagination

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -23,7 +23,7 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <li class="page-item active" aria-current="page"><span class="page-link" style="background-color: #D7CAA0; border: 1px solid #D7CAA0">{{ $page }}</span></li>
                         @else
                             <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                         @endif
