2d172dfebb71f2776e392daa8c44c25be30c6950
Stephanus8889 <stephanus888889@gmail.com>
2022-11-13 23:48:27 +0700

    Update Template Blade

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href={{URL::asset('style.css')}}>
@@ -15,19 +14,20 @@
   </head>
   <body class="content__body">
     @if(auth()->user()->role_id == 1)
-        <div class="side_bar active">
+    <div class="side_bar" style="width: 80px;">
+        <div class="content_bar">
             <div class="side_logo text-center hidden">
                 <h3 style="font-weight: 1000;">STOCKUP</h3>
             </div>
             <ul class="menu_bar" id="menu_bar" style="width: 100%;">
                 <li class="beranda_">
                     <a href="/beranda" id="linkBeranda">
-                            <div class="menu_brnd d-flex ps-4 pt-2 pb-1" role="button" style="transition: all 0.8s;">
+                            <div class="menu_brnd d-flex ps-4 pt-2 pb-1" role="button" style="transition: all 0.3s;">
                                 <div class="iconmenu">
                                     <img src="{{URL::asset('beranda_icon.png')}}" alt="">
                                 </div>
                                 <div class="textmenu">
-                                    <p id="brnd" style="text-decoration:none;">Beranda</p>
+                                    <p id="brnd" class="subtextmenu" style="text-decoration:none;">Beranda</p>
                                 </div>
                             </div>
                     </a>
@@ -35,12 +35,12 @@
 
                 <li class="tambahbarang_">
                     <a href="/tambahbarang" class="linkTambahBarang">
-                        <div class="menu_tmbhbrg d-flex ps-4 pt-2 pb-1" style="width: 100%;" role="button" style="transition: all 0.8s;">
+                        <div class="menu_tmbhbrg d-flex ps-4 pt-2 pb-1" role="button" style="transition: all 0.8s;">
                             <div class="iconmenu">
                                 <img src="{{URL::asset('plus.png')}}" alt="">
                             </div>
                             <div class="textmenu">
-                                <p id="tmbhBrg" style="text-decoration:none;">Tambah Barang</p>
+                                <p id="tmbhBrg" class="subtextmenu" style="text-decoration:none;">Tambah Barang</p>
                             </div>
                         </div>
                     </a>
@@ -48,12 +48,12 @@
 
                 <li class="laporan_">
                     <a href="/laporan" class="linkLaporan">
-                        <div class="menu_lprn d-flex ps-4 pt-2 pb-1" role="button">
+                        <div class="menu_lprn d-flex ps-4 pt-2 pb-1" role="button" style="transition: all 0.8s;">
                             <div class="iconmenu">
                                 <img src="{{URL::asset('laporan.png')}}" alt="">
                             </div>
                             <div class="textmenu">
-                                <p id="lprn" style="text-decoration:none;">Laporan</p>
+                                <p id="lprn" class="subtextmenu" style="text-decoration:none;">Laporan</p>
                             </div>
                         </div>
                     </a>
@@ -61,12 +61,12 @@
 
                 <li class="mengelolabarang_">
                     <a href="/mengelolabarang" class="linkMengelolaBarang">
-                        <div class="menu_mnglola d-flex ps-4 pt-2 pb-1" role="button">
+                        <div class="menu_mnglola d-flex ps-4 pt-2 pb-1" role="button" style="transition: all 0.8s;">
                             <div class="iconmenu">
                                 <img src="{{URL::asset('mengelolabarang.png')}}" alt="">
                             </div>
                             <div class="textmenu">
-                                <p id="mnglolaBrg" style="text-decoration:none;">Mengelola Barang</p>
+                                <p id="mnglolaBrg" class="subtextmenu" style="text-decoration:none;">Mengelola Barang</p>
                             </div>
                         </div>
                     </a>
@@ -79,13 +79,14 @@
                                 <img src="{{URL::asset('toko.png')}}" alt="">
                             </div>
                             <div class="textmenu">
-                                <p id="tko" style="text-decoration:none;">Toko</p>
+                                <p id="tko" class="subtextmenu" style="text-decoration:none;">Toko</p>
                             </div>
                         </div>
                     </a>
                 </li>
             </ul>
-        </div>
+        </div> 
+    </div>
     @elseif(auth()->user()->role_id == 2)
     <div class="side_bar active">
             <div class="side_logo text-center hidden">
